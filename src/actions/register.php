1b653562a1d9fc7385419527e48650a64065d053
Challnet <challnet@mail.ru>
2025-10-05 17:45:58 +0500

    Added config.php

--- src/actions/register.php
+++ src/actions/register.php
@@ -1,2 +1,20 @@
-<?php 
-echo "Hello, world!";
+<?php
+
+session_start();
+require_once "../config.php";
+
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$passwordConfirmation = $_POST["password_confirmation"];
+
+//Validation
+$_SESSION["validation"] = [];
+
+if (empty($username)) {
+  $_SESSION["validation"]["username"] = "Username is empty";
+}
+
+if (!empty($_SESSION["validation"])) {
+  header(header: "Location: " . BASE_URL . "/register.php" );
+}
---
+++ src/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASE_URL", "/auth-system");
+
+// Database constants
+define("DB_HOST", "localhost");
+define("DB_NAME", "auth-system");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
